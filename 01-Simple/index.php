3ae53114fdcf4bbc48711a946ea1162ff15af0fd
manish9707 <manish.9707@gnindia.dronacharya.info>
2017-06-14 23:30:42 -0700

    Create index.php

--- 01-Simple/index.php
+++ 01-Simple/index.php
@@ -2,8 +2,10 @@
 // index.php 20150101 - 20170302
 // Copyright (C) 2015-2017 Mark Constable <markc@renta.net> (AGPL-3.0)
 
+
 echo new class
 {
+    // private class
     private
     $in = [
         'm'     => 'home',      // Method action
